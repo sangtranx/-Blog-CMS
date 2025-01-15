c804171f60f7b3d52b864d4d5a5d3f22b6829bee
sang-tran <sang.tran@enish.com>
2025-01-15 15:01:08 +0700

    adding app_response

---
+++ common/app_response.go
@@ -0,0 +1,19 @@
+package common
+
+type SuccessRes struct {
+	Data   interface{} `json:"data"`
+	Paging interface{} `json:"paging,omitempty"`
+	Filter interface{} `json:"filter,omitempty"`
+}
+
+func NewSuccessResponse(data, paging, filter interface{}) *SuccessRes {
+	return &SuccessRes{
+		Data:   data,
+		Paging: paging,
+		Filter: filter,
+	}
+}
+
+func SimpleSuccessResponse(data interface{}) *SuccessRes {
+	return &SuccessRes{data, nil, nil}
+}
