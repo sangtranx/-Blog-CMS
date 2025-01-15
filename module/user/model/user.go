bcb6347e4ed286e843006c1bc33156b667ccd063
sang-tran <sang.tran@enish.com>
2025-01-15 16:32:43 +0700

    update

---
+++ module/user/model/user.go
@@ -0,0 +1,60 @@
+package usermodel
+
+import (
+	"Blog-CMS/common"
+	"errors"
+)
+
+const EntityName = "User"
+
+type User struct {
+	common.SQLModel `json:",inline"`
+	Email           string `json:"email" gorm:"column:email"`
+	Password        string `json:"password" gorm:"column:password"`
+	Salt            string `json:"-" gorm:"column:salt"`
+	LastName        string `json:"last_name" gorm:"column:last_name"`
+	FirstName       string `json:"first_name" gorm:"column:first_name"`
+	Phone           string `json:"phone" gorm:"column:phone"`
+	Role            string `json:"role" gorm:"column:role"`
+}
+
+func (u *User) GetUserId() int {
+	return u.Id
+}
+
+func (u *User) GetEmail() string {
+	return u.Email
+}
+
+func (u *User) GetRole() string {
+	return u.Role
+}
+
+func (User) TableName() string {
+	return "users"
+}
+
+type UserCreate struct {
+	common.SQLModel `json:",inline"`
+	Email           string `json:"email" gorm:"column:email"`
+	Password        string `json:"password" gorm:"column:password"`
+	Salt            string `json:"-" gorm:"column:salt"`
+	LastName        string `json:"last_name" gorm:"column:last_name"`
+	FirstName       string `json:"first_name" gorm:"column:first_name"`
+	Phone           string `json:"phone" gorm:"column:phone"`
+	Role            string `json:"role" gorm:"column:role"`
+}
+
+func (UserCreate) TableName() string { return User{}.TableName() }
+
+var (
+	ErrEmailnameOrPasswordInvalid = common.NewCustomError(
+		errors.New("email or password invalid"),
+		"email or password invalid",
+		"ErrEmailnameOrPasswordInvalid")
+
+	ErrEmailExisted = common.NewCustomError(
+		errors.New("email has already existed"),
+		"email has already existed",
+		"ErrEmailExisted")
+)
