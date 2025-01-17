435430ab5614a517249fb755074f041083cadcf5
sang-tran <sang.tran@enish.com>
2025-01-17 14:34:27 +0700

    adding goose to managed the migration

---
+++ migrations/20250117072749_create_users_table_up.sql
@@ -0,0 +1,25 @@
+-- +goose Up
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
+
+-- +goose StatementBegin
+CREATE TABLE users (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   email VARCHAR(255) NOT NULL UNIQUE,
+   password VARCHAR(255) NOT NULL,
+   salt VARCHAR(255) NOT NULL,
+   last_name VARCHAR(255),
+   first_name VARCHAR(255),
+   phone VARCHAR(20),
+   role VARCHAR(50),
+   status INT DEFAULT 1,
+   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
