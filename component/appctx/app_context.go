c503b139b7572765352a563a84a298cd59b00064
David <David@Thanhs-MacBook-Pro.local>
2025-02-09 13:36:44 +0700

    combine pubsub and async job

--- component/appctx/app_context.go
+++ component/appctx/app_context.go
@@ -2,6 +2,7 @@ package appctx
 
 import (
 	"Blog-CMS/component/package/logger"
+	"Blog-CMS/component/pubsub"
 	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
@@ -10,6 +11,7 @@ type AppContext interface {
 	GetMainDBConnection() *gorm.DB
 	GetRedisDBConnection() *redis.Client
 	GetLogger() *logger.LoggerZap
+	GetPubsub() pubsub.Pubsub
 	SecretKey() string
 }
 
@@ -17,14 +19,16 @@ type appCtx struct {
 	db        *gorm.DB
 	rdb       *redis.Client
 	logger    *logger.LoggerZap
+	pubsub    pubsub.Pubsub
 	secretKey string
 }
 
-func NewAppContext(db *gorm.DB, rdb *redis.Client, logger *logger.LoggerZap, secretKey string) *appCtx {
+func NewAppContext(db *gorm.DB, rdb *redis.Client, logger *logger.LoggerZap, pubsub pubsub.Pubsub, secretKey string) *appCtx {
 	return &appCtx{
-		db: db,
-		rdb: rdb,
-		logger: logger,
+		db:        db,
+		rdb:       rdb,
+		logger:    logger,
+		pubsub:    pubsub,
 		secretKey: secretKey,
 	}
 }
@@ -32,4 +36,5 @@ func NewAppContext(db *gorm.DB, rdb *redis.Client, logger *logger.LoggerZap, sec
 func (ctx *appCtx) GetMainDBConnection() *gorm.DB       { return ctx.db }
 func (ctx *appCtx) GetRedisDBConnection() *redis.Client { return ctx.rdb }
 func (ctx *appCtx) GetLogger() *logger.LoggerZap        { return ctx.logger }
+func (ctx *appCtx) GetPubsub() pubsub.Pubsub            { return ctx.pubsub }
 func (ctx *appCtx) SecretKey() string                   { return ctx.secretKey }
---
+++ component/subscriber/sub.go
@@ -0,0 +1,66 @@
+package subscriber
+
+import (
+	"Blog-CMS/component/appctx"
+	"Blog-CMS/component/asyncjob"
+	"Blog-CMS/component/pubsub"
+	"context"
+	"log"
+)
+
+type GroupJob interface {
+	Run(ctx context.Context) error
+}
+
+type comsumerJob struct {
+	Title   string
+	Handler func(ctx context.Context, msg *pubsub.Message) error
+}
+
+type consumerEngine struct {
+	appCtx appctx.AppContext
+}
+
+func NewEngine(appCtx appctx.AppContext) *consumerEngine { return &consumerEngine{appCtx: appCtx} }
+
+func (e *consumerEngine) StartSubTopic(topic pubsub.Topic, isConcurrent bool, comsumerJobs ...comsumerJob) error {
+
+	c, _ := e.appCtx.GetPubsub().Subscribe(context.Background(), topic)
+
+	for _, job := range comsumerJobs {
+		log.Println(job.Title)
+	}
+
+	getJobHld := func(job *comsumerJob, msg *pubsub.Message) asyncjob.JobHandler {
+		return func(ctx context.Context) error {
+			return job.Handler(ctx, msg)
+		}
+	}
+
+	go func() {
+		for {
+			msg := <-c
+
+			jobHldArr := make([]asyncjob.Job, len(comsumerJobs))
+
+			for i, job := range comsumerJobs {
+				jobhld := getJobHld(&job, msg)
+				jobHldArr[i] = asyncjob.NewJob(jobhld)
+			}
+
+			g := asyncjob.NewGroup(isConcurrent, jobHldArr...)
+
+			if err := g.Run(context.Background()); err != nil {
+				log.Println(err)
+			}
+		}
+	}()
+
+	return nil
+}
+
+// add new subscribers
+func (e *consumerEngine) Start() error {
+
+	return nil
+}
