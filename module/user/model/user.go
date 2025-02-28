6ac8ff8e7c0331ec5e30258bdc037e95e3d6a10d
sang <tran.thanhsang.dev@gmail.com>
2025-02-28 09:47:34 +0700

    refactor user create model

--- module/user/model/user.go
+++ module/user/model/user.go
@@ -53,9 +53,6 @@ type UserCreate struct {
 	Email           string `json:"email" gorm:"column:email"`
 	Password        string `json:"password" gorm:"column:password"`
 	Salt            string `json:"-" gorm:"column:salt"`
-	LastName        string `json:"last_name" gorm:"column:last_name"`
-	FirstName       string `json:"first_name" gorm:"column:first_name"`
-	Phone           string `json:"phone" gorm:"column:phone"`
 	Role            string `json:"role" gorm:"column:role"`
 }
 
